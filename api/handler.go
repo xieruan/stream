797a4f04feda969cc0c7a78cbc688d1fc90a9170
xieruan <59883140+xieruan@users.noreply.github.com>
2024-05-16 11:32:22 +0800

    Update handler.go

--- api/handler.go
+++ api/handler.go
@@ -37,6 +37,26 @@ func handleCreate(w http.ResponseWriter, r *http.Request) {
 
 	write(w, fmt.Sprintf("FAIL: %s\n", addr))
 }
+func handleList(w http.ResponseWriter, r *http.Request) {
+	list := List()
+	response := strings.Join(list, "\n")
+	write(w, response)
+}
+
+func handleDelete(w http.ResponseWriter, r *http.Request) {
+	addr := r.URL.Query().Get("addr")
+	if addr == "" {
+		write(w, "FAIL: No Address Provided for Deletion\n")
+		return
+	}
+
+	if Delete(addr) {
+		write(w, fmt.Sprintf("DONE: Deleted %s\n", addr))
+		return
+	}
+
+	write(w, fmt.Sprintf("FAIL: Unable to Delete %s\n", addr))
+}
 
 func handlePurge(w http.ResponseWriter, r *http.Request) {
 	Purge()
